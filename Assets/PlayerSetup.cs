d829260c88a4e6789a00052c96f64e609093837b
ee14b104 <sachinagrawal272@gmail.com>
2020-11-20 18:14:56 +0100

    :arrow_up: Added Parrelsync, :sparkles: started working on networking

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using Mirror;
+
+
+public class PlayerSetup : NetworkBehaviour
+
+{
+    [SerializeField] Behaviour[] componentsToDisable ;
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i< componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+    }
+}
