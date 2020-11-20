e56b1285024696d574a77bec06e215ab37da0ee4
ee14b104 <sachinagrawal272@gmail.com>
2020-11-20 16:50:05 +0100

    :ok_hand: reverse camera sensitivity

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float lookSensitivity = 3f;
-    [SerializeField] private float cameraSensitivity = 3f;
+    [SerializeField] private float cameraSensitivity = -3f;
 
     private PlayerMotor motor;
     private void Start()
